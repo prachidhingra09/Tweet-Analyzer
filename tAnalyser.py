1945780f86eb373b52315a0ffa4b0f2af81cbab2
Ashfaaq Mohammed <iim.ashfaaq@gmail.com>
2020-04-15 13:11:58 +0530

    Added util script and stats script for prerequisites

--- tAnalyser.py
+++ tAnalyser.py
@@ -1,9 +1,5 @@
-#import matplotlib.pyplot as plt 
-#import seaborn as sns
-#mport string
-#import warnings 
-#warnings.filterwarnings("ignore", category=DeprecationWarning)
-
+import utils
+import random
 import re # for regular expressions
 import pandas as pd 
 pd.set_option("display.max_colwidth", 200)
@@ -17,11 +13,17 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 
+from sklearn.tree import DecisionTreeClassifier
+from scipy.sparse import lil_matrix
+from sklearn.feature_extraction.text import TfidfTransformer
+
+''' Built by: iimashfaaq & prachidhingra09 '''
 
 # FILES
 TRAIN_FILE  = pd.read_csv('./datasets/train_tweets.csv')
 TEST_FILE = pd.read_csv('./datasets/test_tweets.csv')
-PROCESSED_FILE = 'processed.csv'
+TRAIN_PROCESSED_FILE = './datasets/TRAIN_PROCESSED_FILE.csv'
+TEST_PROCESSED_FILE = './datasets/TEST_PROCESSED_FILE.csv'
 
 # TRUE WHILE TRAINING
 TRAIN = True
@@ -109,7 +111,7 @@ def get_feature_vector(tweet):
     uni_feature_vector = []
     bi_feature_vector = []
     words = tweet.split()
-    for i in xrange(len(words) - 1):
+    for i in range(len(words) - 1):
         word = words[i]
         next_word = words[i + 1]
         if unigrams.get(word):
@@ -125,7 +127,7 @@ def get_feature_vector(tweet):
 
 def extract_features(tweets, batch_size=500, test_file=True, feat_type='presence'):
     num_batches = int(np.ceil(len(tweets) / float(batch_size)))
-    for i in xrange(num_batches):
+    for i in range(num_batches):
         batch = tweets[i * batch_size: (i + 1) * batch_size]
         features = lil_matrix((batch_size, VOCAB_SIZE))
         labels = np.zeros(batch_size)
@@ -210,7 +212,7 @@ def save_processed_file(linesList, processed_file_name, csv_file_name, test_file
     #     else:
     #         save_to_file.write('%s,%s\n' % (tweet_id, line))
     with open(csv_file_name, 'r') as csv:
-        lines = csv.readlines()
+        lines = csv.readlines()[1:]
         total = len(lines)
         for i, line in enumerate(lines):
             tweet_id = line[:line.find(',')]
@@ -261,8 +263,9 @@ if __name__ == "__main__":
     
     print(wordsList[:3])
 
-    save_processed_file(wordsList[:31963], './datasets/TRAIN_PROCESSED_FILE.csv', TRAIN_FILE, False)
-    save_processed_file(wordsList[31964:], './datasets/TEST_PROCESSED_FILE.csv', TEST_FILE, True)
+    # FUNCTION CALL TO SAVE THE PROCESSED DATA INTO CSV FILES
+    save_processed_file(wordsList[:31963], './datasets/TRAIN_PROCESSED_FILE.csv', './datasets/train_tweets.csv', False)
+    save_processed_file(wordsList[31962:], './datasets/TEST_PROCESSED_FILE.csv', './datasets/test_tweets.csv', True)
     # vect = vector_transformation(wordsList)
     # x_train, x_test, y_train, y_test = train_test_splitting(vect)
     
@@ -270,3 +273,59 @@ if __name__ == "__main__":
     #accuracy = accuracy_score(y_test,y_pred)
     #print('\nAccuracy Score-\n')
     #print(accuracy)
+
+    np.random.seed(1337)
+    unigrams = utils.top_n_words('./datasets/train-tweets-freqdist.pkl', UNIGRAM_SIZE)
+    if USE_BIGRAMS:
+        bigrams = utils.top_n_bigrams('./datasets/train-tweets-freqdist-bi.pkl', BIGRAM_SIZE)
+    tweets = process_tweets(TRAIN_PROCESSED_FILE, test_file=False)
+    if TRAIN:
+        train_tweets, val_tweets = utils.split_data(tweets)
+    else:
+        random.shuffle(tweets)
+        train_tweets = tweets
+    del tweets
+    print('Extracting features & training batches')
+    clf = DecisionTreeClassifier(max_depth=25)
+    batch_size = len(train_tweets)
+    i = 1
+    n_train_batches = int(np.ceil(len(train_tweets) / float(batch_size)))
+    for training_set_X, training_set_y in extract_features(train_tweets, test_file=False, feat_type=FEAT_TYPE, batch_size=batch_size):
+        utils.write_status(i, n_train_batches)
+        i += 1
+        if FEAT_TYPE == 'frequency':
+            tfidf = apply_tf_idf(training_set_X)
+            training_set_X = tfidf.transform(training_set_X)
+        clf.fit(training_set_X, training_set_y)
+    print('\n')
+    print('Testing')
+    if TRAIN:
+        correct, total = 0, len(val_tweets)
+        i = 1
+        batch_size = len(val_tweets)
+        n_val_batches = int(np.ceil(len(val_tweets) / float(batch_size)))
+        for val_set_X, val_set_y in extract_features(val_tweets, test_file=False, feat_type=FEAT_TYPE, batch_size=batch_size):
+            if FEAT_TYPE == 'frequency':
+                val_set_X = tfidf.transform(val_set_X)
+            prediction = clf.predict(val_set_X)
+            correct += np.sum(prediction == val_set_y)
+            utils.write_status(i, n_val_batches)
+            i += 1
+        print('\nCorrect: %d/%d = %.4f %%' % (correct, total, correct * 100. / total))
+    else:
+        del train_tweets
+        test_tweets = process_tweets(TEST_PROCESSED_FILE, test_file=True)
+        n_test_batches = int(np.ceil(len(test_tweets) / float(batch_size)))
+        predictions = np.array([])
+        print('Predicting batches')
+        i = 1
+        for test_set_X, _ in extract_features(test_tweets, test_file=True, feat_type=FEAT_TYPE):
+            if FEAT_TYPE == 'frequency':
+                test_set_X = tfidf.transform(test_set_X)
+            prediction = clf.predict(test_set_X)
+            predictions = np.concatenate((predictions, prediction))
+            utils.write_status(i, n_test_batches)
+            i += 1
+        predictions = [(str(j), int(predictions[j])) for j in range(len(test_tweets))]
+        utils.save_results_to_csv(predictions, 'decisionTreeResult.csv')
+        print('\nSaved to decisionTreeResult.csv')
